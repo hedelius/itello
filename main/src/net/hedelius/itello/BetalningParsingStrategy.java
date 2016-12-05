a70259105ab515d0089a1d69b63143c901da31f2
Mathias Hedelius <mathias.hedelius@cybercom.se>
2016-12-05 09:09:45 +0100

    Changed from streams to readLine

--- main/src/net/hedelius/itello/BetalningParsingStrategy.java
+++ main/src/net/hedelius/itello/BetalningParsingStrategy.java
@@ -3,19 +3,16 @@ package net.hedelius.itello;
 import se.itello.example.payments.PaymentReceiver;
 import java.io.*;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Implementation of <code>ParsingStrategy</code> that handles files
  * that conform to specification in the document Betalningsservice.doc.
- * Do not use this class for large files - it reads the entire file into memory
- * before processing it!
  */
 public class BetalningParsingStrategy implements ParsingStrategy {
 
@@ -25,57 +22,79 @@ public class BetalningParsingStrategy implements ParsingStrategy {
     @Override
     public void handle(InputStream input, PaymentReceiver paymentReceiver) throws PaymentException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName(CHARSET)));
+        int expectedPaymentLineCount = 0;
+        int actualPaymentLineCount = 0;
+        BigDecimal actualPaymmentLineAmountTotal = new BigDecimal(0);
+        BigDecimal expectedPaymentAmountTotal = new BigDecimal(0);
 
-        String[] lines = reader.lines().toArray(x -> new String[x]);
-        String[] orderLines = Arrays.stream(lines).filter(x -> x.startsWith("O")).toArray(x -> new String[x]);
-        String orderLine = lines[0];
-
-        if (!orderLine.startsWith("O")) {
-            throw new PaymentException("First line is not a header line!");
-        }
-
-        if (orderLines.length != 1) {
-            throw new PaymentException("Incorrect number of header lines!");
-        }
-
-        String accountNumber = orderLine.substring( 1, 16);
-        String sum           = orderLine.substring(16, 30);
-        String count         = orderLine.substring(30, 40);
-        String dateString    = orderLine.substring(40, 48);
-        String currency      = orderLine.substring(48, 51);
-
-        Date date;
         try {
-            date = DATEFORMAT.parse(dateString);
-        } catch (ParseException e) {
-            throw new PaymentException(e);
+
+            String theOrderLine = null;
+
+            String currentLine;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName(CHARSET)));
+            while ((currentLine = reader.readLine()) != null) {
+
+                // order line?
+                if (currentLine.startsWith("O")) {
+
+                    if (theOrderLine != null) {
+                        throw new PaymentException("Multiple order lines!");
+                    }
+
+                    String accountNumber = currentLine.substring( 1, 16);
+                    String sum           = currentLine.substring(16, 30);
+                    String count         = currentLine.substring(30, 40);
+                    String dateString    = currentLine.substring(40, 48);
+                    String currency      = currentLine.substring(48, 51);
+
+                    theOrderLine = currentLine;
+                    expectedPaymentAmountTotal = bigDecimalFromString(sum);
+                    expectedPaymentLineCount = Integer.parseInt(count.trim());
+
+                    Date date;
+                    try {
+                        date = DATEFORMAT.parse(dateString);
+                    } catch (ParseException e) {
+                        throw new PaymentException(e);
+                    }
+
+                    paymentReceiver.startPaymentBundle(
+                            accountNumber.trim().replace(' ', '-'),
+                            date,
+                            currency);
+                } else {
+
+                    if (theOrderLine == null) {
+                        throw new PaymentException("First line is not an order line!");
+                    }
+
+                    // payment line?
+                    if (currentLine.startsWith("B")) {
+
+                        BigDecimal amount = bigDecimalFromString(currentLine.substring(1, 15));
+                        actualPaymmentLineAmountTotal = actualPaymmentLineAmountTotal.add(amount);
+                        ++actualPaymentLineCount;
+
+                        paymentReceiver.payment(amount, currentLine.substring(15, 50).trim());
+
+                    } else {
+                        throw new PaymentException("Unknown line type!");
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            throw new PaymentException("Failed to read file!");
         }
 
-        paymentReceiver.startPaymentBundle(
-                accountNumber.trim().replace(' ', '-'),
-                date,
-                currency);
-
-        List<String> payments =
-                Arrays.stream(lines, 1, lines.length).filter(x -> x.startsWith("B")).collect(Collectors.toList());
-
-        if (payments.size() != Integer.parseInt(count.trim())) {
+        if (actualPaymentLineCount != expectedPaymentLineCount) {
             throw new PaymentException("Incorrect number of payment records compared to header!");
         }
 
-        BigDecimal recordSum = payments.stream().
-                map(x -> bigDecimalFromString(x.substring(1, 15))).
-                reduce((x, y) -> x.add(y)).get();
-
-        if (!recordSum.equals(bigDecimalFromString(sum))){
+        if (!actualPaymmentLineAmountTotal.equals(expectedPaymentAmountTotal)) {
             throw new PaymentException("Incorrect sum of payments compared to header!");
         }
-
-        payments.forEach(x -> {
-            BigDecimal amount = bigDecimalFromString(x.substring(1, 15));
-            paymentReceiver.payment(amount, x.substring(15, 50).trim());
-        });
 
         paymentReceiver.endPaymentBundle();
     }
